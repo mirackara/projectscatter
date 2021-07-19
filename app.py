cabeb49867de4e6573e6894c18d1a1ff0c1ff714
Andrew Hossack <andrew_hossack@outlook.com>
2021-07-19 16:02:03 -0700

    PEP8 Formatting

--- app.py
+++ app.py
@@ -8,32 +8,34 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///showDatabase.db'
 
 
-
-class showInfo(Resource): # Resource Class that handles GET/PUT/POST Requests
-    def get(self,showName):
+class showInfo(Resource):  # Resource Class that handles GET/PUT/POST Requests
+    def get(self, showName):
         statusCode = 0
-        try: # Show Found!
+        try:  # Show Found!
             seasons = 0
             bestEpName = ""
             worstEpName = ""
             bestEpRating = 0.0
             worstEpRating = 0.0
-            showData, seasons, bestEpName , bestEpRating, worstEpName, worstEpRating = getShowData(showName,seasons)
+            showData, seasons, bestEpName, bestEpRating, worstEpName, worstEpRating = getShowData(
+                showName, seasons)
             statusCode = 200
             headers = {'Content-Type': 'text/html'}
-            return make_response(render_template('home.html', statusCode = statusCode, showData = showData, seasons = seasons, bestEpName = bestEpName, bestEpRating = bestEpRating, worstEpName = worstEpName, worstEpRating = worstEpRating),200,headers)
-        except: # Show not found..
+            return make_response(render_template('home.html', statusCode=statusCode, showData=showData, seasons=seasons, bestEpName=bestEpName, bestEpRating=bestEpRating, worstEpName=worstEpName, worstEpRating=worstEpRating), 200, headers)
+        except:  # Show not found..
             print("error")
             headers = {'Content-Type': 'text/html'}
             statusCode = 404
-            return make_response(render_template('home.html',statusCode = statusCode, showData = {}, seasons = 0),200,headers)
-    
-    def put(self,showName):
-        print(request.form['Schitt\'s Creek']) 
+            return make_response(render_template('home.html', statusCode=statusCode, showData={}, seasons=0), 200, headers)
+
+    def put(self, showName):
+        print(request.form['Schitt\'s Creek'])
         return {}
-    
+
 
 api.add_resource(showInfo, "/search/<string:showName>")
+
+
 @app.route('/')
 def index():
     seasons = 0
@@ -41,73 +43,81 @@ def index():
     worstEpName = ""
     bestEpRating = 0.0
     worstEpRating = 0.0
-    showData, seasons , bestEpName , bestEpRating, worstEpName, worstEpRating = getShowData("Schitt's Creek",seasons)
+    showData, seasons, bestEpName, bestEpRating, worstEpName, worstEpRating = getShowData(
+        "Schitt's Creek", seasons)
     statusCode = 200
-    return render_template('home.html', statusCode = statusCode, showData = showData, seasons = seasons , bestEpName = bestEpName, bestEpRating = bestEpRating, worstEpName = worstEpName, worstEpRating = worstEpRating)
+    return render_template('home.html', statusCode=statusCode, showData=showData, seasons=seasons, bestEpName=bestEpName, bestEpRating=bestEpRating, worstEpName=worstEpName, worstEpRating=worstEpRating)
 
 
 @app.route('/search')
 def search():
     return 'Search'
+
+
 showNames = []
 
 
-
-class showInfoCompare(Resource): # Resource Class that handles GET/PUT/POST Requests
-    def get(self,showName):
+class showInfoCompare(Resource):  # Resource Class that handles GET/PUT/POST Requests
+    def get(self, showName):
         statusCode = 0
-        try: # Show Found!
+        try:  # Show Found!
             print(showName)
-            showNames = showName.split('+') 
-            while("" in showNames) :
+            showNames = showName.split('+')
+            while("" in showNames):
                 showNames.remove("")
             print(showNames)
 
             testList = []
             numOfShows = 0
             for show in showNames:
-                numOfShows+= 1
+                numOfShows += 1
                 seasons = 0
                 bestEpName = ""
                 worstEpName = ""
                 bestEpRating = 0.0
                 worstEpRating = 0.0
-                showData, seasons, bestEpName, worstEpName, bestEpRating, worstEpRating = getShowData(show,seasons)
+                showData, seasons, bestEpName, worstEpName, bestEpRating, worstEpRating = getShowData(
+                    show, seasons)
                 testList.append(deepcopy(showData))
 
-            return make_response(render_template('multigraph.html', test = testList, numOfShows = numOfShows))
-        except: # Show not found..
+            return make_response(render_template('multigraph.html', test=testList, numOfShows=numOfShows))
+        except:  # Show not found..
             print("error")
             headers = {'Content-Type': 'text/html'}
             statusCode = 404
-            return make_response(render_template('home.html',statusCode = statusCode, showData = {}, seasons = 0),200,headers)
-    
-    def put(self,showName):
-        print(request.form['Schitt\'s Creek']) 
+            return make_response(render_template('home.html', statusCode=statusCode, showData={}, seasons=0), 200, headers)
+
+    def put(self, showName):
+        print(request.form['Schitt\'s Creek'])
         return {}
-    
+
 
 api.add_resource(showInfoCompare, "/compare/<string:showName>")
+
+
 @app.route('/')
 def indexCompare():
     testList = []
     numOfShows = 0
     for show in showNames:
-        numOfShows+= 1
+        numOfShows += 1
         seasons = 0
         bestEpName = ""
         worstEpName = ""
         bestEpRating = 0.0
         worstEpRating = 0.0
-        showData, seasons, bestEpName , bestEpRating, worstEpName, worstEpRating = getShowData(show,seasons)
+        showData, seasons, bestEpName, bestEpRating, worstEpName, worstEpRating = getShowData(
+            show, seasons)
         testList.append(deepcopy(showData))
 
-    return make_response(render_template('multigraph.html', test = testList, numOfShows = numOfShows))
+    return make_response(render_template('multigraph.html', test=testList, numOfShows=numOfShows))
+
 
 @app.route('/compare')
 def compare():
     return 'compare'
 
-#Start Web App
+
+# Start Web App
 if __name__ == '__main__':
     app.run(debug=True)
