3de397e425eb93762b81e063b4acfa20b425fc81
Mirac Kara <mkara3@uic.edu>
2021-12-13 21:34:50 -0600

    V0.5.1

--- projectscatter/settings.py
+++ projectscatter/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^$ujmt!gvd=84#60h0s(%mh1cuyoo7jerx57^i6wizs_smi%zt'
+SECRET_KEY = 'djangoSecret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Leave Blank for debugging
