2fefa527abb7b4f98e4c66a64dce1ebd20ee9849
Mirac Kara <mkara3@uic.edu>
2021-12-13 20:39:16 -0600

    Azure Static Files Fix

--- projectscatter/urls.py
+++ projectscatter/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 # search function
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('scatterbox.urls')),
     path('', include('scatterbox.urls')),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
